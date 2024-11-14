c29dce7642c7e22984d2b4ae310d6cc38e5e5bbf
Harrison Correa Florez <Harrisoncorreaflorez@gmail.com>
2024-11-13 21:42:42 -0500

    Corrección del nombre de la clase

--- CalcularGastos.java
+++ CalcularGastos.java
@@ -1,6 +1,6 @@
 // Ejercicio 4 - Clase 02 - JAVA DEVSENIOR
 
-public class CacularGastos {
+public class CalcularGastos {
     public static void main(String[] args) {
         float valorInicial = 23.0f;
         float valorFinal = 12.75f;
